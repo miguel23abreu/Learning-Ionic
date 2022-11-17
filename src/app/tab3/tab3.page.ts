0be3d0e255ada2d1e9b664ca5d92994b2670414d
miguel abreu <guelabreu@hotmail.com>
2022-11-17 01:26:48 -0300

    projeto ionic concluido

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-  constructor() {}
-  whats(){
-    this.abrirUrl('https://api.whatsapp.com/send?phone=555194904309&text=Ol%C3%A1%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes');
+export class Tab3Page implements OnInit {
+
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(){
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
   }
 
   abrirUrl(url: string){
     window.open(url, '_blank').focus();
+  }
+
+  enviar(){
+    let nome = this.form.get('nome').value;
+    let endereco = this.form.get('endereco').value;
+    console.log(nome);
+
+    // eslint-disable-next-line max-len
+    this.abrirUrl(`https://api.whatsapp.com/send?phone=555194904309&text=Ola%20me%20chamo%20${nome}%20moro%20em%20${endereco}%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes`);
   }
 }
